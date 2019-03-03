3e9e07e3f7a791e20f9aff47f1e0ae2c43c670b7
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2019-03-03 15:26:28 +0000

    step 8 complete

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -32,22 +33,24 @@ def about():
 def login():
     form = LoginForm()
     if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
 
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
+        if form.validate_on_submit():
 
-            # get user id, load into session
-            login_user(user)
+            username = form.username.data
+            password = form.password.data
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            user = UserProfile.query.filter_by(username=username).first()
+            
+            if user is not None and check_password_hash(user.password, password):
+                # get user id, load into session
+                login_user(user)
+
+                flash('Log in successful.', 'success')
+                next_page = request.args.get('next')
+                return redirect(url_for("secure_page"))
+            else:
+                flash('Username or Password is incorrect.', 'danger')
+
     return render_template("login.html", form=form)
 
 
